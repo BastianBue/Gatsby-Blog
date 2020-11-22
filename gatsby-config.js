b01accbcaea2d5f5dba1d8f15885f0df4ef8198e
Bastian Büld <66347304+BastianBue@users.noreply.github.com>
2020-11-22 18:31:51 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    title: "Getsby Bootcamp",
+    author: "Bastian Büld",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
+}
